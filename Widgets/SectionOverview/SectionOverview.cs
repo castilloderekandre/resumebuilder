ad6216ccfc9fd6a8d6388a7d06ae56bd326c5fff
castilloderekandre <castilloderekandre@gmail.com>
2026-04-10 15:56:48 -0600

    Added label controls for SectionOverview widget

---
+++ Widgets/SectionOverview/SectionOverview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeBuilder.Widgets.SectionOverview;
+
+internal class SectionOverview : Control
+{
+    Label label1 = new Label()
+    {
+        Text = "Sections",
+    };
+    Label label2 = new Label()
+    {
+        Text = "Preview",
+    };
+    Label label3 = new Label()
+    {
+        Text = "Editor",
+    };
+    Label label4 = new Label();
+}
