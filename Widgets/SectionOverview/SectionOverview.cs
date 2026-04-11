2278dba4bbfa8f95aa8c330220e0095a0abfab04
castilloderekandre <castilloderekandre@gmail.com>
2026-04-10 18:20:34 -0600

    Added basic GUI controls for section overview widget

--- Widgets/SectionOverview/SectionOverview.cs
+++ Widgets/SectionOverview/SectionOverview.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResumeBuilder.Widgets.ItemPickerControl;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,15 +9,57 @@ internal class SectionOverview : Control
 {
     Label label1 = new Label()
     {
-        Text = "Sections",
+        Text = "Preview",
     };
     Label label2 = new Label()
     {
-        Text = "Preview",
+        Text = "Section Editor",
     };
     Label label3 = new Label()
     {
-        Text = "Editor",
+        Text = "Section Title",
     };
-    Label label4 = new Label();
+
+    Button button1 = new Button()
+    {
+
+    };
+    Button button2 = new Button()
+    {
+
+    };
+    Button button3 = new Button()
+    {
+
+    };
+
+    PictureBox pictureBox1 = new PictureBox()
+    {
+
+    };
+
+    TextBox textBox1 = new TextBox()
+    {
+
+    };
+
+    ItemPicker itemPicker1 = new ItemPicker(null)
+    {
+
+    };
+
+    public SectionOverview()
+    {
+        Controls.AddRange(
+                label1,
+                label2,
+                label3,
+                button1,
+                button2,
+                button3,
+                pictureBox1,
+                textBox1,
+                itemPicker1
+            );
+    }
 }
