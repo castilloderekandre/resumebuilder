9678b45fd84cd5530fc8362a6efaa1cdba0dbc0a
castilloderekandre <castilloderekandre@gmail.com>
2026-04-16 14:42:23 -0600

    NTree class partially implemented

---
+++ NTreeSuff/NTreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeBuilder.NTreeSuff
+{
+    internal class NTreeNode<T>
+    {
+        public NTreeNode<T>? Parent;
+        public T? Data;
+        public List<NTreeNode<T>>? Children;
+
+        public NTreeNode()
+        {
+
+        }
+    }
+}
